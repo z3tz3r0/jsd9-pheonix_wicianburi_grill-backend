2f97bfb1bfaf93aa1ba960bf33af4feb91baec95
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-08 16:49:24 +0700

    fix(cors): makes the index.html available

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const whitelist = [
 ];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
